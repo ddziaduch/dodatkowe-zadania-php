2e5c39412ab26ea080a855a921383d6185dd0ff6
Damian Dziaduch <damian@dziaduch.pl>
2022-03-24 20:53:55 +0100

    Add tests

--- tests/Refactoring/Dietary/TaxRuleServiceTest.php
+++ tests/Refactoring/Dietary/TaxRuleServiceTest.php
@@ -7,6 +7,7 @@ namespace LegacyFighter\Dietary\Tests;
 use LegacyFighter\Dietary\Repository\InMemoryOrderRepository;
 use LegacyFighter\Dietary\Repository\InMemoryTaxConfigRepository;
 use LegacyFighter\Dietary\Repository\InMemoryTaxRuleRepository;
+use LegacyFighter\Dietary\TaxConfig;
 use LegacyFighter\Dietary\TaxRule;
 use LegacyFighter\Dietary\TaxRuleService;
 use PHPUnit\Framework\TestCase;
@@ -20,6 +21,19 @@ final class TaxRuleServiceTest extends TestCase
      */
     private $taxRuleService;
 
+    public function testCanAddLinearTaxRuleToCountryThatHaveNoRulesYet(): void
+    {
+        $this->taxRuleService->addTaxRuleToCountry(self::COUNTRY_CODE, 3, 4, 'efg');
+
+        $this->assertRulesCount(1);
+        $this->assertRuleIsLinear(
+            3,
+            4,
+            'A. 899. 2022efg',
+            $this->getLastRule()
+        );
+    }
+
     public function testCanAddLinearTaxRuleToCountry(): void
     {
         $this->aTaxConfigWithRules(1);
@@ -27,7 +41,12 @@ final class TaxRuleServiceTest extends TestCase
         $this->taxRuleService->addTaxRuleToCountry(self::COUNTRY_CODE, 1, 2, 'abc');
 
         $this->assertRulesCount(2);
-        $this->assertLastRuleIsLinear(1, 2, 'A. 899. 2022abc');
+        $this->assertRuleIsLinear(
+            1,
+            2,
+            'A. 899. 2022abc',
+            $this->getLastRule()
+        );
     }
 
     public function testCannotAddLinearTaxRuleToCountryWithZeroAsFactorA(): void
@@ -46,15 +65,7 @@ final class TaxRuleServiceTest extends TestCase
         $this->taxRuleService->addTaxRuleToCountry(self::COUNTRY_CODE, 1, 2, 'abc');
     }
 
-    public function testCanAddLinearTaxRuleToCountryThatHaveNoRulesYet(): void
-    {
-        $this->taxRuleService->addTaxRuleToCountry(self::COUNTRY_CODE, 3, 4, 'efg');
-
-        $this->assertRulesCount(1);
-        $this->assertLastRuleIsLinear(3, 4, 'A. 899. 2022efg');
-    }
-
-    public function canCreateTaxConfigWithCustomMaxRuleCount(): void
+    public function testCanCreateTaxConfigWithCustomMaxRuleCount(): void
     {
         $this->taxRuleService->createTaxConfigWithRuleAndMaxCount(self::COUNTRY_CODE, 15, new TaxRule());
         $this->aTaxConfigWithRules(14);
@@ -63,27 +74,63 @@ final class TaxRuleServiceTest extends TestCase
         $this->taxRuleService->addTaxRuleToCountry(self::COUNTRY_CODE, 1, 1, 'abc');
     }
 
-    private function aTaxConfigWithRules(int $numberOfRules): void
+    public function testCanAddSquareTaxRuleToCountryThatHaveNoRulesYet(): void
     {
-        for ($i = 0; $i < $numberOfRules; $i++) {
-            $this->taxRuleService->addTaxRuleToCountry(self::COUNTRY_CODE, $i, $i, (string) $i);
-        }
+        $this->taxRuleService->addTaxRuleToCountry2(self::COUNTRY_CODE, 5, 6, 7, 'xyz');
+
+        $this->assertRulesCount(2);
+        $rules = $this->getRules();
+        $this->assertRuleIsSquare(
+            5,
+            6,
+            7,
+            'A. 899. 2022xyz',
+            $rules[0]
+        );
+        // wtf, this is definitely a bug!
+        $this->assertRuleIsSquare(
+            5,
+            6,
+            7,
+            'A. 899. 2022xyz',
+            $rules[1]
+        );
     }
 
-    private function assertRulesCount(int $count): void
+    public function testCanAddSquareTaxRuleToCountry(): void
     {
-        $this->assertSame($count, $this->taxRuleService->rulesCount(self::COUNTRY_CODE));
+        $this->aTaxConfigWithRules(1);
+
+        $this->taxRuleService->addTaxRuleToCountry2(self::COUNTRY_CODE, 8, 9, 10, 'ccc');
+
+        $rules = $this->getRules();
+        $this->assertRuleIsSquare(
+            8,
+            9,
+            10,
+            'A. 899. 2022ccc',
+            $rules[1]
+        );
     }
 
-    private function assertLastRuleIsLinear(int $factorA, int $factorB, string $taxCode): void
+    public function testCanDeleteRuleIfCountryHasMoreThanOneRule(): void
     {
-        $rules = $this->taxRuleService->findRules(self::COUNTRY_CODE)->toArray();
-        $rule = end($rules);
-        assert($rule instanceof TaxRule);
-        self::assertTrue($rule->isLinear());
-        self::assertSame($factorA, $rule->getaFactor());
-        self::assertSame($factorB, $rule->getbFactor());
-        self::assertSame($taxCode, $rule->getTaxCode());
+        $this->aTaxConfigWithRules(2);
+
+        $rule = $this->getLastRule();
+        $taxConfig = $this->getTaxConfig();
+        $this->taxRuleService->deleteRule($rule->getId(), $taxConfig->getId());
+        self::assertCount(1, $this->getRules());
+    }
+
+    public function testCannotDeleteLastRule(): void
+    {
+        $this->aTaxConfigWithRules(1);
+
+        $rule = $this->getLastRule();
+        $taxConfig = $this->getTaxConfig();
+        $this->expectExceptionObject(new \Exception('Last rule in country config'));
+        $this->taxRuleService->deleteRule($rule->getId(), $taxConfig->getId());
     }
 
     protected function setUp(): void
@@ -95,5 +142,67 @@ final class TaxRuleServiceTest extends TestCase
         $taxRuleRepository = new InMemoryTaxRuleRepository($taxConfigRepository);
 
         $this->taxRuleService = new TaxRuleService($taxRuleRepository, $taxConfigRepository, $orderRepository);
+    }
+
+    private function aTaxConfigWithRules(int $numberOfRules): void
+    {
+        for ($i = 1; $i <= $numberOfRules; $i++) {
+            $this->taxRuleService->addTaxRuleToCountry(self::COUNTRY_CODE, $i, $i, (string) $i);
+        }
+    }
+
+    private function assertRulesCount(int $count): void
+    {
+        $this->assertSame($count, $this->taxRuleService->rulesCount(self::COUNTRY_CODE));
+    }
+
+    private function assertRuleIsLinear(
+        int $factorA,
+        int $factorB,
+        string $taxCode,
+        TaxRule $rule
+    ): void {
+        self::assertTrue($rule->isLinear());
+        self::assertSame($factorA, $rule->getaFactor());
+        self::assertSame($factorB, $rule->getbFactor());
+        self::assertSame($taxCode, $rule->getTaxCode());
+    }
+
+    /**
+     * @return TaxRule[]
+     */
+    private function getRules(): array
+    {
+        return $this->taxRuleService->findRules(self::COUNTRY_CODE)->toArray();
+    }
+
+    private function getLastRule(): TaxRule
+    {
+        $rules = $this->getRules();
+        $rule = end($rules);
+        assert($rule instanceof TaxRule);
+
+        return $rule;
+    }
+
+    private function assertRuleIsSquare(
+        int $expectedFactorA,
+        int $expectedFactorB,
+        int $expectedFactorC,
+        string $expectedTaxCode,
+        TaxRule $rule
+    ): void {
+        self::assertTrue($rule->isSquare());
+        self::assertSame($expectedFactorA, $rule->getaSquareFactor());
+        self::assertSame($expectedFactorB, $rule->getbSquareFactor());
+        self::assertSame($expectedFactorC, $rule->getcSquareFactor());
+        self::assertSame($expectedTaxCode, $rule->getTaxCode());
+    }
+
+    private function getTaxConfig(): TaxConfig
+    {
+        $configs = $this->taxRuleService->findAllConfigs();
+
+        return end($configs);
     }
 }
